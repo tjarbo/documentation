cd1126109528ae1cdf8301b44bbc65e43a5b0f5d
tjarbo <16938041+tjarbo@users.noreply.github.com>
2021-08-24 22:17:20 +0200

    🚧 Updated sidebar

--- docs/moodlebot_versioned_sidebars.js
+++ docs/moodlebot_versioned_sidebars.js
@@ -14,13 +14,43 @@ module.exports = {
     "home",
     {
       type: 'category',
-      label: 'Setup your bot',
+      label: 'Getting started',
       items: [
-        'setup-preparation',
-        'setup-with-docker',
-        'setup-with-heroku-and-mongodb-atlas',
+        'getting-started/',
       ],
     },
-    "what-is-inside-env",
+    {
+      type: 'category',
+      label: 'Guides',
+      items: [
+      ],
+    },
+    {
+      type: 'category',
+      label: 'Advanced Guides',
+      items: [
+        'advanced-guides/configure-via-env',
+        'advanced-guides/what-is-inside-env',
+      ],
+    },
+    {
+      type: 'category',
+      label: 'Let\'s contribute',
+      items: [
+        'contribution/i-want-to-code',
+        'contribution/translations',
+        'contribution/i-want-to-update-wiki',
+      ],
+    },
+    {
+      type: 'link',
+      label: 'FAQ',
+      href: 'https://github.com/tjarbo/discord-moodle-bot/discussions/categories/q-a',
+    },
+    {
+      type: 'link',
+      label: 'Matrix Channel',
+      href: '',
+    }
   ],
 };
